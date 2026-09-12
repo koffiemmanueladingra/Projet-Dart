3c4eeda6415e5b78d302979c02430d80aaad670e
kadingra <emmanuel.adingra@xsel-services.com>
2026-09-12 19:29:55 +0000

    mise en ligne du projet sur gtihub

--- task_cli/bin/main.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:io';
-
-import 'package:task_cli/src/cli/cli_app.dart';
-
-void main(List<String> arguments) {
-  final app = CliApp();
-  exitCode = app.run(arguments);
-}
--- task_cli/lib/src/repository/repository.dart
+++
@@ -1,9 +0,0 @@
-abstract class Repository<T> {
-  List<T> getAll();
-
-  T? getById(String id);
-
-  void add(T item);
-
-  void delete(String id);
-}
